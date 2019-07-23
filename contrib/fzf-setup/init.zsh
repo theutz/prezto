3c900690f42f9d27bf779687aa867738644339e2
Michael Utz <michael@theutz.com>
2019-07-23 20:50:51 +0300

    Use fd in fzf

--- contrib/fzf-setup/init.zsh
+++ contrib/fzf-setup/init.zsh
@@ -1,1 +1,3 @@
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git --nocolor -g ""'
+export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git --color=always"
+export FZF_DEFAULT_OPTS="--ansi"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
