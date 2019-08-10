922f6639461fae9905c146aa8cdd96a61f306a91
Michael Utz <michael@theutz.com>
2019-08-10 21:58:14 +0300

    Make sure mac-specific aliases don't end up in termux

--- contrib/custom-aliases/init.zsh
+++ contrib/custom-aliases/init.zsh
@@ -1,26 +1,29 @@
-# lsd
-alias ls="lsd"
+# NOT APPLICABLE IN TERMUX
+if [[ ! -v ANDROID_DATA ]]; then
+  # lsd
+  alias ls="lsd"
+
+  # cat
+  alias cat="bat"
+
+  # top
+  alias top="htop"
+
+  # spotify
+  alias s="spotify"
+
+  # brew bundle
+  alias brewb="brew bundle"
+  alias brewbd="brew bundle dump"
+  alias brewbD="brew bundle dump --force"
+  alias brewbc="brew bundle cleanup"
+  alias brewbC="brew bundle cleanup --force"
+  alias brewbi="brew bundle install --no-upgrade"
+  alias brewbu="brew bundle install"
+fi
 
 # git
 alias giaa="git add -A"
-
-# cat
-alias cat="bat"
-
-# top
-alias top="htop"
-
-# spotify
-alias s="spotify"
-
-# brew bundle
-alias brewb="brew bundle"
-alias brewbd="brew bundle dump"
-alias brewbD="brew bundle dump --force"
-alias brewbc="brew bundle cleanup"
-alias brewbC="brew bundle cleanup --force"
-alias brewbi="brew bundle install --no-upgrade"
-alias brewbu="brew bundle install"
 
 # misc
 alias todaymd="nvim `date +%Y-%m-%d`.md"
