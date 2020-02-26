0489a10d63b2bf78bbbbabd8bd89b33d829f0858
Michael Utz <michael@theutz.com>
2020-02-26 16:51:16 +0200

    Update custom aliases

--- contrib/custom-aliases/init.zsh
+++ contrib/custom-aliases/init.zsh
@@ -1,29 +1,21 @@
-# NOT APPLICABLE IN TERMUX
-if [[ ! -v ANDROID_DATA ]]; then
-  # lsd
-  alias ls="lsd"
+# lsd
+alias ls="lsd"
 
-  # cat
-  alias cat="bat"
+# cat
+alias cat="bat"
 
-  # top
-  alias top="htop"
-
-  # spotify
-  alias s="spotify"
-
-  # brew bundle
-  alias brewb="brew bundle"
-  alias brewbd="brew bundle dump"
-  alias brewbD="brew bundle dump --force"
-  alias brewbc="brew bundle cleanup"
-  alias brewbC="brew bundle cleanup --force"
-  alias brewbi="brew bundle install --no-upgrade"
-  alias brewbu="brew bundle install"
-fi
+# brew bundle
+alias brewb="brew bundle"
+alias brewbd="brew bundle dump"
+alias brewbD="brew bundle dump --force"
+alias brewbc="brew bundle cleanup"
+alias brewbC="brew bundle cleanup --force"
+alias brewbi="brew bundle install --no-upgrade"
+alias brewbu="brew bundle install"
 
 # git
 alias giaa="git add -A"
 
-# misc
-alias todaymd="nvim `date +%Y-%m-%d`.md"
+alias ess="brew services start emacs-plus"
+alias esx="brew services stop emacs-plus"
+alias esS="brew services restart emacs-plus"
