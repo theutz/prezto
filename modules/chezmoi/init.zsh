5870aef5a99f0913c36cfd1926180720cddd1041
Michael Utz <michael@theutz.com>
2023-09-29 17:34:53 +0300

    feat(chezmoi): Add support for sourcing secret files on startup

--- modules/chezmoi/init.zsh
+++ modules/chezmoi/init.zsh
@@ -2,4 +2,13 @@ if ! zstyle -t ':prezto:module:chezmoi:brew' override; then
   unfunction brew
 fi
 
-eval "$(chezmoi completion zsh)"
+typeset -a secret_files
+zstyle -a ':prezto:module:chezmoi:secrets' files 'secret_files'
+for secret_file in "${(k)secret_files[@]}"; do
+  if [[ -f "$secret_file" ]]; then
+    source $secret_file
+  elif [[ -f "$(chezmoi target-path)/$secret_file" ]]; then
+    source "$(chezmoi target-path)/$secret_file"
+  fi
+done
+unset secret_file{,s}
