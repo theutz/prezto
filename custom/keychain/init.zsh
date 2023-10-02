334a46c077646403ee0b547e864e852562b01d45
Michael Utz <michael@theutz.com>
2023-10-02 18:17:26 +0300

    style(keychain): Remove unnecessary array join operator

--- custom/keychain/init.zsh
+++ custom/keychain/init.zsh
@@ -1,5 +1,5 @@
 zstyle -a ':prezto:module:keychain' keys 'keychain_keys'
-zstyle -a ':prezto:module:keychain' agents 'keychain_agents'
+zstyle -s ':prezto:module:keychain' agents 'keychain_agents' ','
 
 local keychain_quiet
 
@@ -7,6 +7,6 @@ if zstyle -t ':prezto:module:keychain' quiet; then
   keychain_quiet="--quiet"
 fi
 
-eval "$(keychain --eval $keychain_quiet --agents "${(j:,:)keychain_agents}" "$keychain_keys[@]")"
+eval "$(keychain --eval $keychain_quiet --agents "$keychain_agents" "$keychain_keys[@]")"
 
 unset keychain_keys keychain_agents keychain_quiet
