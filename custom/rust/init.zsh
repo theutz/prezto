262fe1cc118ef83164702bea294a75a9e2951189
Michael Utz <michael@theutz.com>
2023-10-05 23:31:02 +0300

    refactor(rust,runcoms): move rust config to module

--- custom/rust/init.zsh
+++ custom/rust/init.zsh
@@ -1,7 +1,11 @@
-. "$HOME/.cargo/env"
-
-export RUST_COMPLETIONS_DIR="${0:a:h}/functions"
+if zstyle -T ':prezto:module:rust:dir' use-xdg; then
+  export CARGO_HOME="$XDG_DATA_HOME"/cargo
+  . "$CARGO_HOME/env"
+else
+  . "$HOME/.cargo/env"
+fi
 
 if zstyle -T ':prezto:modules:rust:completion' enable; then
+  export RUST_COMPLETIONS_DIR="${0:a:h}/functions"
   setup-rust-completion
 fi
