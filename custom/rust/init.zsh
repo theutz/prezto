c180bb69d3706d74f018a6b0af8f6c187bcf52dc
Michael Utz <michael@theutz.com>
2023-10-05 23:48:41 +0300

    fix(rust): path for rustup
    Signed-off-by: Michael Utz <michael@theutz.com>

--- custom/rust/init.zsh
+++ custom/rust/init.zsh
@@ -1,9 +1,13 @@
 if zstyle -T ':prezto:module:rust:dir' use-xdg; then
   export CARGO_HOME="$XDG_DATA_HOME"/cargo
+  export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
   . "$CARGO_HOME/env"
 else
   . "$HOME/.cargo/env"
 fi
+
+# Prepend cargo bin to path
+path=("${CARGO_HOME:-$HOME/cargo}/bin" $path)
 
 if zstyle -T ':prezto:modules:rust:completion' enable; then
   export RUST_COMPLETIONS_DIR="${0:a:h}/functions"
