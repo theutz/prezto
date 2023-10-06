6d8d590d8b7ebc2aba5a650590547cf7ea4a5294
Michael Utz <michael@theutz.com>
2023-10-06 14:47:39 +0300

    fix(rust): don't assume rust is in system

--- custom/rust/init.zsh
+++ custom/rust/init.zsh
@@ -1,13 +1,15 @@
-if zstyle -T ':prezto:module:rust:dir' use-xdg; then
+if [[ -z "$CARGO_HOME" ]]; then
   export CARGO_HOME="$XDG_DATA_HOME"/cargo
-  export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
-  . "$CARGO_HOME/env"
-else
-  . "$HOME/.cargo/env"
 fi
 
+if [[ -z "$RUSTUP_HOME" ]]; then
+  export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+fi
+
+[[ -s "$CARGO_HOME/env" ]] && . "$CARGO_HOME/env"
+
 # Prepend cargo bin to path
-path=("${CARGO_HOME:-$HOME/cargo}/bin" $path)
+path=("${CARGO_HOME}/bin" $path)
 
 if zstyle -T ':prezto:modules:rust:completion' enable; then
   export RUST_COMPLETIONS_DIR="${0:a:h}/functions"
