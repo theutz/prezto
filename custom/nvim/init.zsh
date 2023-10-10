bc7da45fa60239bb518195b011127b50babd830b
Michael Utz <michael@theutz.com>
2023-10-10 14:12:34 -0700

    feat(nvim): Enable bob neovim version manager and use neovim as manpage viewer

---
+++ custom/nvim/init.zsh
@@ -0,0 +1,9 @@
+if zstyle -T ':prezto:modules:nvim:bob' enable; then
+  export path=("$XDG_DATA_HOME/bob/nvim-bin" $path)
+  eval "$(bob complete nvim)"
+fi
+
+if zstyle -T ':prezto:modules:nvim:man' enable; then
+  export MANPAGER='nvim +Man!'
+  export MANWIDTH='999'
+fi
