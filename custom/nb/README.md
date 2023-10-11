119614e46e707911eef374a7f127119809e9b2e7
Michael Utz <michael@theutz.com>
2023-10-11 12:20:32 -0700

    feat(nb): new nb module
    Signed-off-by: Michael Utz <michael@theutz.com>

---
+++ custom/nb/README.md
@@ -0,0 +1,3 @@
+# nb
+
+Aliases and other support for [nb](https://xwmx.github.io/nb/).
---
+++ custom/nb/init.zsh
@@ -0,0 +1,30 @@
+if zstyle -T ':prezto:modules:nb:server' autostart; then
+  if (( $+commands[tmux] )) && ! tmux -L nb has-session -t nb &> /dev/null; then
+    tmux -L nb new-session -d -s nb -c $HOME "nb browse -s"
+  fi
+fi > /dev/null
+
+alias nba='nb add'
+alias nbb='nb bookmark'
+alias nbbls='nb bookmark ls'
+alias nbcp='nb copy'
+alias nbd='nb do'
+alias nbD='nb undo'
+alias nbf='nb folders'
+alias nbg='nb git'
+alias nbla='nb list'
+alias nbls='nb ls'
+alias nbm='nb pin' # mark
+alias nbM='nb unpin' # unmark
+alias nbmv='nb move'
+alias nbnb='nb notebooks'
+alias nbo='nb open'
+alias nbp='nb peek'
+alias nbs='nb shell'
+alias nbt='nb todo'
+alias nbta='nb todo add'
+alias nbts='nb todos'
+alias nbtt='nb todo tasks'
+alias nbu='nb use'
+alias nbw='nb browse'
+alias nbx='nb delete'
