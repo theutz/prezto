20e75304ce53618eb8b98578293f916efeae25fd
Michael Utz <michael@theutz.com>
2023-10-11 08:22:50 -0700

    feat(nvim): Update bob completion to zsh

--- custom/neovim/init.zsh
+++ custom/neovim/init.zsh
@@ -1,6 +1,6 @@
 if zstyle -T ':prezto:modules:nvim:bob' enable; then
   export path=("$XDG_DATA_HOME/bob/nvim-bin" $path)
-  eval "$(bob complete nvim)"
+  eval "$(bob complete zsh)"
 fi
 
 if zstyle -T ':prezto:modules:nvim:man' enable; then
