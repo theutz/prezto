bbc6158d2dc12cc96e96a66b8c8ae8b80e383c13
Michael Utz <michael@theutz.com>
2023-10-11 13:55:27 -0700

    feat(zoxide): add cd aliases
    Signed-off-by: Michael Utz <michael@theutz.com>

--- custom/zoxide/init.zsh
+++ custom/zoxide/init.zsh
@@ -2,4 +2,12 @@ if ! (( $+commands[zoxide] )); then
   return 1
 fi
 
+# Load zoxide
 eval "$(zoxide init zsh)"
+
+if zstyle -t ':prezto:module:zoxide:alias-cd' enable; then
+  alias cd='z'
+fi
+
+alias cdi="zi"
+
