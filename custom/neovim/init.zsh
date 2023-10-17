4d1c69ed39260e5df6db5f3ea55189532505887d
Michael Utz <michael@theutz.com>
2023-10-17 08:45:35 -0700

    fix(bob): still test for command before running
    Signed-off-by: Michael Utz <michael@theutz.com>

--- custom/neovim/init.zsh
+++ custom/neovim/init.zsh
@@ -1,6 +1,8 @@
 if zstyle -T ':prezto:modules:nvim:bob' enable; then
   export path=("$XDG_DATA_HOME/bob/nvim-bin" $path)
-  eval "$(bob complete zsh)"
+  if command -v bob &> /dev/null; then
+    eval "$(bob complete zsh)"
+  fi
 fi
 
 if zstyle -T ':prezto:modules:nvim:man' enable; then
