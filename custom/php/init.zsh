69eae5f2beb1a380bbf4e98805567e371b24150d
Mike Utz <mike.u@delegator.com>
2024-03-15 22:27:25 +0300

    fix: better php aliases
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- custom/php/init.zsh
+++ custom/php/init.zsh
@@ -1,3 +1,10 @@
 if command -v composer &> /dev/null; then
   path=("$(composer config --global home)/$(composer config --global bin-dir)" $path)
 fi
+
+# Aliases
+alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
+alias art='[ -f artisan ] && php artisan'
+alias pls='[ -f please ] && php artisan'
+
+alias wp='sh $([ -f wp ] && echo wp || echo vendor/bin/wp)'
