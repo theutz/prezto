304bbac9fe53a3fa5911600894baf00d17573065
Mike Utz <mike.u@delegator.com>
2024-03-19 12:55:42 +0300

    feat: make `a` function aware of sail
    Signed-off-by: Mike Utz <mike.u@delegator.com>

--- custom/php/init.zsh
+++ custom/php/init.zsh
@@ -4,7 +4,4 @@ fi
 
 # Aliases
 alias sail='sh $([ -f sail ] && echo sail || echo vendor/bin/sail)'
-alias art='[ -f artisan ] && php artisan'
-alias pls='[ -f please ] && php artisan'
-
 alias wp='sh $([ -f wp ] && echo wp || echo vendor/bin/wp)'
