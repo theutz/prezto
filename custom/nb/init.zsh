f9217daa5c7ccc963a316ac07300093cf0ec3eb5
Michael Utz <michael@theutz.com>
2024-07-31 12:55:27 +0300

    chore: add window name for nb session
    Signed-off-by: Michael Utz <michael@theutz.com>

--- custom/nb/init.zsh
+++ custom/nb/init.zsh
@@ -1,6 +1,6 @@
 if zstyle -T ':prezto:modules:nb:server' autostart; then
   if (( $+commands[tmux] )) && ! tmux -L nb has-session -t nb &> /dev/null; then
-    tmux -L nb new-session -d -s nb -c $HOME "nb browse -s"
+    tmux -L nb new-session -d -s nb -n main -c $HOME "nb browse -s"
   fi
 fi > /dev/null
 
