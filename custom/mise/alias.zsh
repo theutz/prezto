40c47039367396b9e18758f3f0e12c0d1d88a9ab
Michael Utz <michael@theutz.com>
2025-12-10 21:22:04 +0300

    chore: add mise

---
+++ custom/mise/alias.zsh
@@ -0,0 +1,4 @@
+alias m='mise'
+alias mr='mise run'
+alias mx='mise exec'
+alias mt='mise task'
---
+++ custom/mise/init.zsh
@@ -0,0 +1,4 @@
+[[ -x $HOME/.local/bin/mise ]] &&
+  eval "$($HOME/.local/bin/mise activate zsh)"
+
+source "${0:a:h}/alias.zsh"
